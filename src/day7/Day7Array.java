61103780872f62062e1393efc0a62bfc3de612f4
rlaEndals <rnwlsdn9621043@gmail.com>
2025-02-03 11:47:57 +0900

    Day7Array

---
+++ src/day7/Day7Array.java
@@ -0,0 +1,36 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Day7Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        char[] chars = new char[4];
+//        chars[0] = 'c';
+//        chars[1] = 'h';
+//        chars[2] = 'a';
+//        chars[3] = 'r';
+//        for (int i = 0; i <= 3; i++) {
+//            System.out.println(chars[i]);
+//        }
+        int[] ints = {85, 90, 78, 92, 88};
+
+        for (int number : ints) {
+            System.out.println("number = " + number);
+        }
+
+//        for (int i = 0; i <= 4; i++) {
+//            System.out.println(ints[i]);
+//        }
+//        System.out.println(ints.length);
+//        int len = sc.nextInt();
+//        int[] ar = new int[len];
+//        for (int i = 0; i < len; i++){
+//            ar[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < ar.length; i++) {
+//            System.out.print(ar[i] + " ");
+//        }
+
+    }
+}
---
+++ src/day7/Day7Practice.java
@@ -0,0 +1,38 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Day7Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        double qt = 0;
+//        System.out.print("학생수를 입력하세요: ");
+//        int st = sc.nextInt();
+//        int[] so = new int[st];
+//        for (int i = 0; i < st; i++){
+//            System.out.print("학생들의 점수를 입력하세요. 학생" + (i + 1) + ": ");
+//            so[i] = sc.nextInt();
+//            qt += so[i];
+//        }
+//
+//        System.out.println("학생들의 평균점수는: " + (qt / st));
+//        System.out.print("숫자개수를 입력하세요: ");
+//        int len = sc.nextInt();
+//        int[] fo = new int[len];
+//        for (int i = 0; i < len; i++){
+//            System.out.print((i + 1) + "번째 숫자를 입력하세요: ");
+//            fo[i] = sc.nextInt();
+//        }
+//        int max = fo[0];
+//        int min = fo[0];
+//        for (int i = 1; i < len; i++){
+//            if (max < fo[i]) {
+//                max = fo[i];
+//            } else if (min > fo[i]) {
+//                min = fo[i];
+//            }
+//        }
+//        System.out.println("min = " + min);
+//        System.out.println("max = " + max);
+    }
+}
