08870521585150218ced6d1ab7027688b93c7b90
rlaEndals <rnwlsdn9621043@gmail.com>
2025-02-10 11:40:24 +0900

    Day9

--- src/day9/Day9Practice.java
+++ src/day9/Day9Practice.java
@@ -22,5 +22,15 @@ public class Day9Practice {
         System.out.println("자동차 색깔 = " + car2.color);
         car2.jajun();
         car2.horn();
+
+        Day9Car car3 = new Day9Car();
+        car3.name = "현대";
+        car3.species = "승용차";
+        car3.color = "검은색";
+        System.out.println("자동차 이름 = " + car3.name);
+        System.out.println("자동차 종 = " + car3.species);
+        System.out.println("자동차 색깔 = " + car3.color);
+        car3.jajun();
+        car3.horn();
     }
 }
