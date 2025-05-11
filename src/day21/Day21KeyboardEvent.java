5bd292bf8457482be0b56f7aa497c308912e07b5
rlatmdals0513 <2011seungmin.k@gmail.com>
2025-05-11 11:23:53 +0900

    day21

---
+++ src/day21/Day21KeyboardEvent.java
@@ -0,0 +1,37 @@
+package day21;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Day21KeyboardEvent {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("키보드 이벤트 예제");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setText("키를 눌러보세요!");
+        textArea.setEditable(false);
+
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                textArea.setText("keyTyped: " + e.getKeyChar());
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                textArea.setText("keyPressed: " + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                textArea.setText("keyReleased: " + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+        });
+
+        frame.add(textArea);
+        frame.setVisible(true);
+    }
+}
---
+++ src/day21/Day21MouseEvent.java
@@ -0,0 +1,64 @@
+package day21;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class Day21MouseEvent {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("마우스 이벤트 예제");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawString("마우스를 클릭하거나 드래그해 보세요!", 50, 50);
+            }
+        };
+
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("mouseClicked:" + e.getPoint());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("mousePressed: " + e.getPoint());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("mouseReleased: " + e.getPoint());
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("mouseEntered");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                System.out.println("mouseExited");
+            }
+        });
+        panel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                System.out.println("mouseDragged: " + e.getPoint());
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                System.out.println("mouseMoved: " + e.getPoint());
+            }
+        });
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
