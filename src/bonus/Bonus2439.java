d7d7d1d37d2eed5a9235b517a440e3c8f3657849
rlaEndals <rnwlsdn9621043@gmail.com>
2025-11-30 11:01:09 +0900

    project reservation

---
+++ src/bonus/Bonus2439.java
@@ -0,0 +1,21 @@
+package bonus;
+
+import java.util.Scanner;
+
+public class Bonus2439 {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int m = i; m<=n; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
